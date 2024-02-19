fbe14c8c3e0ca8aa782001d1a1e82659a6427c54
gangan <44604921+shinGangan@users.noreply.github.com>
2024-02-19 14:59:18 +0900

    feat: add `Nuxt Sitemap` (#23)
    * feat: add `Nuxt Sitemap` (#7)
    * docs: add Nuxt Sitemap badge (#7)

--- packages/nuxt.config.ts
+++ packages/nuxt.config.ts
@@ -1,9 +1,10 @@
 export default defineNuxtConfig({
-  modules: ["@nuxt/image", "@nuxtjs/i18n"],
+  modules: ["@nuxt/image", "@nuxtjs/i18n", "@nuxtjs/sitemap"],
   image: {
     quality: 80,
     format: ["webp"],
     dir: "assets/images",
   },
   i18n: {},
+  site: {},
 });
